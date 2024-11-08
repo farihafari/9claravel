973b5329c3dfd95a906c722349a4a3aa4e3b09cb
farihafari <fareehajabeen62@gmail.com>
2024-11-08 12:53:37 +0500

    laravel form

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('panel.index');
 });
-Route::get("home",function(){
-    return view("index");
+Route::get("category",function(){
+    return view("panel.category");
 });
